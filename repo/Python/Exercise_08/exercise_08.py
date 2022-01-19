aaed62ec2afacb312831318f3289afe5f7a16ccd
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2022-01-19 14:52:56 +0400

    Exercise_08

---
+++ repo/Python/Exercise_08/exercise_08.py
@@ -0,0 +1,6 @@
+first_number = int(input("Enter a number: "))
+second_number = int(input("Enter a number: "))
+third_number = int(input("Enter a number: "))
+total = first_number + second_number + third_number
+average = (first_number + second_number + third_number) / 3
+print("Sum of three numbers is",total,",average of three numbers is",round(average,2))
