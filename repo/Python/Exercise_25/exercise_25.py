e3aafe45fe9943347027d24fccd25d3cfd4b0a6f
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2022-01-19 14:58:55 +0400

    Exercise_25

---
+++ repo/Python/Exercise_25/exercise_25.py
@@ -0,0 +1,5 @@
+#!usr/bin/evn python3
+
+number = int(input("Enter a four-digit number: "))
+digit = str(number)
+print(digit[0], "+", digit[1], "+", digit[2], "+", digit[3], "=", int(digit[0]) + int(digit[1]) + int(digit[2]) + int(digit[3]))
