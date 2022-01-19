cd7006c012586f6764f07c5fd287d55b7d9acdf7
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2022-01-19 15:18:59 +0400

    Exercise1_01

---
+++ repo/Python/Exercise1_01/exercise_01.py
@@ -0,0 +1,6 @@
+#!usr/bin/evn python3
+
+rows = int(input("Enter the number of rows of the box: "))
+columns = int(input("Enter the number of columns of the box: "))
+for i in range(rows):
+        print("*" * columns)
