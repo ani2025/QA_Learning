954e5610e91903c3028bbbe142a4de63d1e7e055
ani2025 <ANIHARUTYUNYAN61@GMAIL.COM>
2022-01-19 14:30:10 +0400

    Exercise_03

---
+++ repo/Python/Exercise_03/exercise_03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print("*" + "\n" + "*" * 2 + "\n" + "*" * 3 +"\n" + "*" * 4)
+
+print("*", "*" * 2, "*" * 3, "*" * 4, sep = "\n")
+
+print("*")
+print("*" * 2)
+print("*" * 3)
+print("*" * 4)
